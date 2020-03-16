be22a0374d792f284008b6aab266bf5a1b4b5ced
ssilnah <ssilyabhannah@gmail.com>
2020-03-16 20:15:42 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="container-fluid">
+          <div class="container">
+              <div class="row">
+              <div class="col-md-6 text-center footer-content">
+                  <p><?php bloginfo('name'); ?></p>
+                  <img src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/tumblr.jpg" alt="tumblr">
+                  <img src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/rss.jpg" alt="rss">
+                  </div>
+                  <div class="col-md-6 text-center footer-content">
+                  <p>Terms and Conditions</p>
+                      <img src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/linkedin.jpg" alt="linkedin">
+                      <img src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/fb.jpg" alt="facebook">
+                  </div>
+              </div>
+          </div>
+      </footer>
+<?php wp_footer(); ?>

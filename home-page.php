d63f3ea60ed597a5a59ecbf2bf33be392f927393
ssilnah <ssilyabhannah@gmail.com>
2020-04-20 15:10:59 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,49 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+      <section class="container-fluid aboutbg text-center">
+      <div class="container">
+          
+<!--placeholder for about me -->
+          
+          
+          
+    <div class="blocker"></div>
+          
+          
+          
+          </div><!--container-->
+      </section>
+      
+      <section class="container-fluid articlebg">
+      <div class="container">
+          <div class="row">
+              <div>
+              <h3 class="text-center article-title">LATEST ARTICLES</h3>
+              </div>
+              <div class="col-md-4 article-cent">
+                  
+<!--placeholder for col1 -->
+                  
+              </div>
+              
+              <div class="col-md-4 article-cent">
+                  
+<!--placeholder for col1 -->
+                  
+              </div>
+              
+              <div class="col-md-4 article-cent">
+                  
+<!--placeholder for col1 -->
+                  
+              </div>
+          </div>
+<!--row-->
+          </div>
+<!--container-->
+      </section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>

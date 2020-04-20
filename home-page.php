e86a5022ccf8900caf9aecf27d4b9b29038ec9b6
ssilnah <ssilyabhannah@gmail.com>
2020-04-20 15:47:14 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -8,9 +8,9 @@
       <div class="container">
           
 <!--placeholder for about me -->
-          
-          
-          
+          <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+          <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+          <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
     <div class="blocker"></div>
           
           

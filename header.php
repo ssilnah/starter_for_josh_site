855d263c2b65d2b758e6649ef53c0bb51890c591
ssilnah <ssilyabhannah@gmail.com>
2020-04-20 16:53:07 +0800

    Update header.php

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
       <?php wp_head(); ?>
     </head>
   <body>
-      <header class="container-fluid herobg">
+      <header class="container-fluid herobg"<?php if ( get_field(' headerbackgroundimage ') ) { echo 'style="background: url(' . get_field(' headerbackgroundimage ') . ')"'; } ?>>
       <nav class="navbar">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -67,7 +67,7 @@
     
 
       <div class="container">
-        <h1> Games Design for the Future</h1>
+        <h1><?php the_field('maintagline'); ?></h1>
     </div>
       </header>
       

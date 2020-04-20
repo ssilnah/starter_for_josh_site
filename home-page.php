dd99a34a6d2e2f8897cbf07497b8870d9d31e1f1
ssilnah <ssilyabhannah@gmail.com>
2020-04-20 16:53:05 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -21,24 +21,34 @@
       <section class="container-fluid articlebg">
       <div class="container">
           <div class="row">
-              <div>
-              <h3 class="text-center article-title">LATEST ARTICLES</h3>
+              <div class="article-title text-center">
+               <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
               </div>
               <div class="col-md-4 article-cent">
                   
+<img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
+<h4><?php the_field('articletitle'); ?></h4>
+<p><?php the_field('articleexcerpt'); ?> </p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
 <!--placeholder for col1 -->
                   
               </div>
               
               <div class="col-md-4 article-cent">
                   
-<!--placeholder for col1 -->
+<img class="img-responsive" alt=“model” src="<?php the_field('articleimage2'); ?>">
+<h4><?php the_field('articletitle2'); ?></h4>
+<p><?php the_field('articleexcerpt2'); ?> </p>
+<a href="<?php the_field('readmore2'); ?>" class="readmore">CONTINUE READING</a>
                   
               </div>
               
               <div class="col-md-4 article-cent">
                   
-<!--placeholder for col1 -->
+<img class="img-responsive" alt=“racetrack” src="<?php the_field('articleimage3'); ?>">
+<h4><?php the_field('articletitle3'); ?></h4>
+<p><?php the_field('articleexcerpt3'); ?> </p>
+<a href="<?php the_field('readmore3'); ?>" class="readmore">CONTINUE READING</a>
                   
               </div>
           </div>
